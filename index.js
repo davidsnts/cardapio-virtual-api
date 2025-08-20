ca62d2ec11e51fbf2baf585126f619410de23bd7
davidsnts <david.santos@delage.com.br>
2025-08-20 19:18:06 -0300

    feat: primeira rota

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const app = express();
 
 app.use(express.json());
 
+app.get('/', (req, res) =>{
+    res.send({"message": "Bem vindo a cardapio-virtual-api"});
+});
+
 app.listen(PORT, () => {
     console.log(`Api executando na porta http://localhost:${PORT} `);
 })
